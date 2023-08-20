e42a3e98d3689055a198e263054ef7ad4fe18565
timDeHof <this.is.tim.dehof@gmail.com>
2023-08-20 08:38:00 -0400

    feat(pageTitle.jsx): create a new component for page titles fix(pageTitle.jsx): fix default value for subtitle prop fix(pageTitle.jsx): fix class name for center alignment fix(pageTitle.jsx): fix class name for subtitle width fix(pageTitle.jsx): fix import statement for SocialIcons component fix(index.js): fix import statement for nextjs icon fix(devto.js): fix portfolioURL variable assignment fix(devto.js): fix convertCanonicalURLToRelative function to only replace blogURL fix(devto.js): fix convertDevtoResponseToArticle function to parse article object fix(devto.js): fix getArticleFromCache function to be async fix(markdown.js): fix import statements for remark-parse, remark-html, rehype-highlight, and remark-gfm fix(markdown.js): fix convertMarkdownToHtml function to split gray-matter from markdown fix(blog.jsx): fix import statement for Head component fix(blog.jsx): fix
    fix([slug].jsx): fix import order and add missing import for PageTitle component
    feat([slug].jsx): add support for dynamic routing with getStaticPaths and getStaticProps
    refactor([slug].jsx): clean up commented out code and error handling in getStaticProps

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,68 +1,135 @@
 import fs from "fs";
+import path from "path";
 import moment from "moment";
 import { GetStaticPaths, GetStaticProps } from "next";
-import path from "path";
 import { getAllBlogArticles, getArticleFromCache } from "../api/devto";
 import Image from "next/image";
-
+import { PageTitle } from "@/components";
 const cacheFile = ".dev-to-cache.json";
 
-export default function Page({ article, publishedDate }) {
-  return (
-    <>
-      {article.coverImage && (
-        <Image
-          src={article.coverImage}
-          alt={`cover of ${article.title}`}
-          title={article.title}
-          width={360}
-          height={202.5}
-          quality={100}
-          className='object-cover w-full h-auto rounded-2xl'
-        />
-      )}
-      <p className='w-full my-4 italic leading-relaxed text-center text-gray-600'>
-        {publishedDate}
-      </p>
-      <section className='flex flex-col items-center w-full mt-6 font-light leading-relaxed'>
-        <article
-          className='w-full text-white lg:prose-lg md:w-5/6 xl:w-9/12'
-          dangerouslySetInnerHTML={{ __html: article.html }}
-        />
-      </section>
-    </>
-  );
-}
+const ArticlePage = ({ article }) => (
+  <>
+    <Image
+      src={article.coverImage}
+      alt={`cover image for ${article.title}`}
+      title={article.title}
+      width={360}
+      height={202.5}
+      quality={100}
+      className='h-40 mx-auto md:mt-6 lg:mt-10 xl:mt-14 sm:h-48 md:h-52 lg:h-64 xl:h-68 2xl:h-80'
+    />
 
-export const getStaticProps = async (context) => {
-  const { slug } = context.params;
+    <PageTitle title={article.title} center icons={false} />
+    <section className='flex flex-col items-center w-full mt-10 font-light leading-relaxed'>
+      <article
+        className='w-full prose text-white max-w-none lg:prose-lg md:w-5/6 xl:w-9/12'
+        dangerouslySetInnerHTML={{ __html: article.html }}
+      />
+    </section>
+  </>
+);
 
-  const cacheContents = fs.readFileSync(
-    path.join(process.cwd(), cacheFile),
-    "utf-8",
-  );
-  let fixedSlug = "/" + slug;
+export const getStaticProps = async ({ params }) => {
+  // Read cache and parse to object
+  let cacheContents;
+  try {
+    cacheContents = fs.readFileSync(
+      path.join(process.cwd(), cacheFile),
+      "utf-8",
+    );
+  } catch (error) {
+    console.error("Error reading cache file:", error);
+    return { notFound: true };
+  }
   const cache = JSON.parse(cacheContents);
 
-  const article = await getArticleFromCache(cache, fixedSlug);
-
-  const publishedDate = moment(article.publishedAt).format("Do MMMM YYYY");
-  return {
-    props: { article, publishedDate },
-  };
+  // Fetch the article from the cache
+  const article = await getArticleFromCache(cache, params.slug);
+  return { props: { article } };
 };
 
-export const getStaticPaths = async () => {
+export async function getStaticPaths() {
+  // Get the published articles and cache them for use in getStaticProps().
   const articles = await getAllBlogArticles();
+
+  // Save articles data to cache file
   fs.writeFileSync(
     path.join(process.cwd(), cacheFile),
     JSON.stringify(articles),
   );
-  // console.log("articles: ", articles);
+
+  // Get the paths we want to pre-render based on posts
   const paths = articles.map(({ slug }) => {
     return {
       params: { slug },
     };
   });
-  return { paths, fallback: true };
-};
+
+  // we'll pre-render only these paths at build time.
+  // {fallback: false} means other routes should 404.
+  return { paths, fallback: false };
+}
+
+export default ArticlePage;
+// const { slug } = params;
+// console.log("slug from API:", slug);
+// // const normalizedSlug = slug.replace("/", "");
+// // console.log("normalized slug:", normalizedSlug);
+// let cacheContents;
+// try {
+//   cacheContents = fs.readFileSync(
+//     path.join(process.cwd(), cacheFile),
+//     "utf-8",
+//   );
+// } catch (error) {
+//   console.error("Error reading cache file:", error);
+//   return { notFound: true };
+// }
+
+// let cache;
+// try {
+//   cache = JSON.parse(cacheContents);
+// } catch (error) {
+//   console.error("Error parsing cache JSON:", error);
+//   return { notFound: true };
+// }
+// const article = await getArticleFromCache(cache, slug);
+// if (!article) {
+//   console.error("No article data fetched for slug:", slug);
+//   return { notFound: true };
+// }
+
+// if (!article || !article.publishedAt) {
+//   console.error("Article not found or missing data for slug:", slug);
+//   return { notFound: true };
+// }
+// if (!article || !article.coverImage) {
+//   console.error("Article not found or missing coverImage for slug:", slug);
+//   return { notFound: true };
+// }
+// let publishedDate;
+// try {
+//   publishedDate = moment(article.publishedAt).format("Do MMMM YYYY");
+// } catch (error) {
+//   console.error("Error formatting published date:", error);
+//   return { notFound: true };
+// }
+// return {
+//   props: { article, publishedDate },
+// };
+// };
+
+// export const getStaticPaths = async () => {
+//   const articles = await getAllBlogArticles();
+//   fs.writeFileSync(
+//     path.join(process.cwd(), cacheFile),
+//     JSON.stringify(articles),
+//   );
+//   // console.log("articles: ", articles);
+//   const paths = articles.map(({ slug }) => {
+//     return {
+//       params: { slug },
+//     };
+//   });
+//   return { paths, fallback: true };
+// };
