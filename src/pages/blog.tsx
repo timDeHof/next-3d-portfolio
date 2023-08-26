8a391cdf773c06116422a9982cbe274505f7d15e
timDeHof <this.is.tim.dehof@gmail.com>
2023-08-26 13:22:37 -0400

    chore:add locale seo meta tags

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -61,7 +61,7 @@ const Blog = ({ articles }) => (
     <Head>
       <meta charSet='UTF-8' />
       <link rel='icon' type='image/svg+xml' href='/logo.svg' />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta name='theme-color' content='#000' />
       <link rel='canonical' href='/blog' />
       <title>Tim DeHof: Blog</title>
