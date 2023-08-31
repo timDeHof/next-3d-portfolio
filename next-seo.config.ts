b0db42a06be0f3f2e202dedb7083587a07b58827
timDeHof <this.is.tim.dehof@gmail.com>
2023-08-31 19:56:17 -0400

    chore(next-seo.config.ts): refactor SEO configuration feat(next-seo.config.ts): add additional link and meta tags add(public): add favicon and other necessary files for SEO
    The SEO configuration in next-seo.config.ts has been refactored to use a constant named SEO of type NextSeoProps. This improves code readability and maintainability.
    
    Additional link tags and meta tags have been added to the SEO configuration. These tags include links to favicon files, a manifest file, and a browserconfig.xml file. The meta tags include the msapplication-TileColor and theme-color.
    
    New favicon files, a browserconfig.xml file, and a site.webmanifest file have been added to the public directory. These files are necessary for proper SEO and display of the website.

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,4 +1,6 @@
-export default {
+import { NextSeoProps } from 'next-seo';
+
+const SEO: NextSeoProps = {
   title: 'Tim DeHof | timdehof.dev',
   description: 'The personal website for Tim DeHof, developer.',
   canonical: 'https://timdehof.dev',
@@ -7,7 +9,47 @@ export default {
     locale: 'en_US',
     title: 'Tim DeHof | timdehof.dev',
     description: 'The personal website for Tim DeHof, developer.',
-    images: [],
+
     site_name: 'Tim DeHof | timdehof.dev',
   },
+  additionalLinkTags: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: '#5bbad5',
+    },
+  ],
+  additionalMetaTags: [
+    {
+      name: 'msapplication-TileColor',
+      content: '#da532c',
+    },
+    {
+      name: 'theme-color',
+      content: '#ffffff',
+    },
+  ],
 };
+
+export default SEO;
