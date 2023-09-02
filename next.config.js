85675220e038d9c854dc6eb82321efbfec44eec3
timDeHof <this.is.tim.dehof@gmail.com>
2023-09-02 19:39:31 -0400

    chore(next.config.js): disable React strict mode The reactStrictMode option in next.config.js has been changed from true to false. This change disables React strict mode in the Next.js application.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
